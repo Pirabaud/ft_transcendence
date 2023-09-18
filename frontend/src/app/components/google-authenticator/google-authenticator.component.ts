eeb362c56c64b532e4cb4ea91bbafe0c856a39ee
remietc <95086567+remietc@users.noreply.github.com>
2023-09-18 20:14:02 +0200

    a detail

--- frontend/src/app/components/google-authenticator/google-authenticator.component.ts
+++ frontend/src/app/components/google-authenticator/google-authenticator.component.ts
@@ -10,7 +10,6 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./google-authenticator.component.css']
 })
 export class GoogleAuthenticatorComponent {
-  private readonly key = 'tfa';
   
   constructor (
     private route: ActivatedRoute,
@@ -38,7 +37,7 @@ export class GoogleAuthenticatorComponent {
           this.httpBackend.verify2fa(obj).subscribe((response2: any) => {
             if (response2) {
               if (response2.success == true) {
-				localStorage.setItem(this.key, "true");
+				localStorage.setItem('tfa', "true");
                 window.location.href = 'home';
               } else {
                 window.alert("Please enter a valid code");
