712ab3570249eadc6c989577573434611ed571aa
LuCXaDs <serbu.lucas@gmail.com>
2023-10-05 16:17:02 +0200

    add file remove/add admin

--- frontend/src/app/components/chat/chat.component.ts
+++ frontend/src/app/components/chat/chat.component.ts
@@ -392,4 +392,8 @@ export class ChatComponent {
   viewProfilUser(id: number) {
     this.router.navigate(['/friendProfile', id.toString()]);
   }
+
+  addAdmin() {
+    
+  }
 }
---
+++ frontend/src/app/components/chat/room_service/remove-admin/remove-admin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-remove-admin',
+  templateUrl: './remove-admin.component.html',
+  styleUrls: ['./remove-admin.component.css']
+})
+export class RemoveAdminComponent {
+
+}
